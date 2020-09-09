0d2ee0ee576cf0e5454d6c582e3bdeaab5c74bbe
phamthaihoang1234 <phamthaihoang1412@gmail.com>
2020-09-09 11:43:04 +0700

    java.js

---
+++ demojava.js
@@ -0,0 +1,12 @@
+let f = 20.5
+let b = true
+let s = 'Hà Nội'
+let i = 2
+
+document.write('i = ' + i)
+document.write('<br/>')
+document.write('f = ' + f)
+document.write('<br/>')
+document.write('b = ' + b)
+document.write('<br/>')
+document.write('s = ' + s)
